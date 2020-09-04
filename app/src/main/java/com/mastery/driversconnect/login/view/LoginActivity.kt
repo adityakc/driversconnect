645e58051424c15689f83588c92a0834d65395ad
adityakc <adityakc@gmail.com>
2020-09-04 00:09:57 -0500

    Adding Launcher activity in mvp format

---
+++ app/src/main/java/com/mastery/driversconnect/login/view/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.mastery.driversconnect.login.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mastery.driversconnect.R
+
+/**
+ * Main User Login Activity and is Launcher Activity as well.
+ */
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+
+}
