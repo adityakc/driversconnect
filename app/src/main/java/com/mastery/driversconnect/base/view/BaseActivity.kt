9533d9c4dfed55ad8be79f38a0266d8c6e781c9e
adityakc <adityakc@gmail.com>
2020-09-05 02:11:54 -0500

    adding progress bar automatically

--- app/src/main/java/com/mastery/driversconnect/base/view/BaseActivity.kt
+++ app/src/main/java/com/mastery/driversconnect/base/view/BaseActivity.kt
@@ -1,12 +1,32 @@
 package com.mastery.driversconnect.base.view
 
-import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
-import com.mastery.driversconnect.R
 
 /**
  * This is the base activity. All activities should inherit from base.
  */
-open class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity(), IBaseView {
+
+    var progressBar: ProgressBar? = null
+
+    abstract fun setUpProgressBar()
+
+    override fun onDataFetchStarted() {
+        progressBar?.visibility = View.VISIBLE
+    }
+
+    override fun onSuccess() {
+        progressBar?.visibility = View.GONE
+    }
+
+    override fun onError(throwable: Throwable) {
+        progressBar?.visibility = View.GONE
+    }
+
+    override fun onConnectionError() {
+        progressBar?.visibility = View.GONE
+    }
 
 }
