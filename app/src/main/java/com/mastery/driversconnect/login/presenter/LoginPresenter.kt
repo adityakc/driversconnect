05c5590d1f0b7454ec22ae6b2a0d6cbbea5b4c96
adityakc <adityakc@gmail.com>
2020-09-05 02:10:22 -0500

    adding logic to presenter

--- app/src/main/java/com/mastery/driversconnect/login/presenter/LoginPresenter.kt
+++ app/src/main/java/com/mastery/driversconnect/login/presenter/LoginPresenter.kt
@@ -1,11 +1,29 @@
 package com.mastery.driversconnect.login.presenter
 
-import com.mastery.driversconnect.base.model.service.BaseService
+import com.mastery.driversconnect.base.presenter.BasePresenter
+import com.mastery.driversconnect.login.model.entity.Credentials
+import com.mastery.driversconnect.login.model.service.ILoginService
+import com.mastery.driversconnect.login.view.ILoginView
 
-class LoginPresenter: BaseService() {
+class LoginPresenter(private val service: ILoginService) : BasePresenter<ILoginView>() {
 
-    fun attemptLogin(){
+    fun attemptLogin(userName: String?, password: String?) {
+        if (!isValid(userName, password)) return
+        val loginFlow = service.callLoginService(Credentials(userName, password))
+        subscribe(loginFlow, { user ->
+            view?.onLoginSuccess(user)
+        }, {
 
+        })
     }
+
+    private fun isValid(userName: String?, password: String?): Boolean {
+        if (userName.isNullOrEmpty() || password.isNullOrEmpty()) {
+            view?.onLoginFailure()
+            return false
+        }
+        return true
+    }
+
 
 }
