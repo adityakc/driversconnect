e49fc5ce2eb68440d17f964adb277cd863193c7e
adityakc <adityakc@gmail.com>
2020-09-05 00:52:13 -0500

    Adding Login Presenter Adding Login Activity

---
+++ app/src/main/java/com/mastery/driversconnect/login/presenter/LoginPresenter.kt
@@ -0,0 +1,11 @@
+package com.mastery.driversconnect.login.presenter
+
+import com.mastery.driversconnect.base.model.service.BaseService
+
+class LoginPresenter: BaseService() {
+
+    fun attemptLogin(){
+
+    }
+
+}
--- app/src/main/java/com/mastery/driversconnect/login/view/LoginActivity.kt
+++ app/src/main/java/com/mastery/driversconnect/login/view/LoginActivity.kt
@@ -3,11 +3,12 @@ package com.mastery.driversconnect.login.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mastery.driversconnect.R
+import com.mastery.driversconnect.base.view.BaseActivity
 
 /**
  * Main User Login Activity and is Launcher Activity as well.
  */
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
