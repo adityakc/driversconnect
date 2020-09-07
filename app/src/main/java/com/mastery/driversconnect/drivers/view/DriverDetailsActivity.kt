f9f5509bfb86eea1106abfceacd260804047ac21
adityakc <adityakc@gmail.com>
2020-09-07 18:10:58 -0500

    setup Details Data

--- app/src/main/java/com/mastery/driversconnect/drivers/view/DriverDetailsActivity.kt
+++ app/src/main/java/com/mastery/driversconnect/drivers/view/DriverDetailsActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.mastery.driversconnect.R
 import com.mastery.driversconnect.constants.IntentKeys
 import com.mastery.driversconnect.drivers.model.entity.Driver
+import kotlinx.android.synthetic.main.activity_driver_details.*
 
 class DriverDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,10 +15,20 @@ class DriverDetailsActivity : AppCompatActivity() {
     }
 
 
-    private fun setUpUI(){
+    private fun setUpUI() {
         val driver = intent.getParcelableExtra<Driver>(IntentKeys.KEY_DRIVER)
         setUpTitle(driver)
+        setUpDetails(driver)
 
+
+    }
+
+    private fun setUpDetails(driver: Driver?) {
+        driver?.details?.apply {
+            text_view_trailer_type_data.text = trailerType
+            text_view_trailer_dim_data.text = "$trailerLength X $trailerWidth X $trailerHeight"
+            text_view_plate_number_data.text = plateNumber
+        }
     }
 
     private fun setUpTitle(driver: Driver?) {
