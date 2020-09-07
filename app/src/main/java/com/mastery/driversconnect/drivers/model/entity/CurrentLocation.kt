37e38e30f717df423eb2e5648caf3ffd6ab1dc3d
adityakc <adityakc@gmail.com>
2020-09-07 17:11:39 -0500

    make driver class parcelable

--- app/src/main/java/com/mastery/driversconnect/drivers/model/entity/CurrentLocation.kt
+++ app/src/main/java/com/mastery/driversconnect/drivers/model/entity/CurrentLocation.kt
@@ -1,8 +1,11 @@
 package com.mastery.driversconnect.drivers.model.entity
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class CurrentLocation(@SerializedName("latitude")
                            val latitude: String = "",
                            @SerializedName("longitude")
-                           val longitude: String = "")
+                           val longitude: String = ""): Parcelable
